711b5fbad529bdf78cdb56b0785d43f93ba0a5d4
subash acharya <suacharya@olivemedia.co>
2017-06-18 12:11:59 +0545

    initial commit

---
+++ Try/resources/views/other/about.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.master')
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <p class="quote">
+                About Me
+            </p>
+        </div>
+        <div class="row"></div>
+        <div class="col-md-12">
+            <p> lorem ipsume khgkajhgak k ghjakhg  khgakga  hgakjgha  khgajgga  ouhgalkjg i gahkgauo  ktoquihga iuhgakga  uouigah a</p>
+        </div>
+
+
+    </div>
+    @endsection
---
+++ Try/resources/views/partials/header.blade.php
@@ -0,0 +1,15 @@
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a href="#" class="navbar-brand">Our sample</a>
+            <ul class="nav navbar-nav">
+                <li><a href="#">blog</a></li>
+                <li><a href="{{route('other.about')}}">about</a></li>
+                <li><a href="#">contact</a></li>
+            </ul>
+
+        </div>
+
+    </div>
+
+</nav>
