6b105a6994179e9c3f87da0aa5908f95dcbd8d88
subash acharya <suacharya@olivemedia.co>
2017-06-18 12:40:14 +0545

    Initial Setup
    Initial Setups

---
+++ resources/views/admin/create.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.admin')
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <form action="" method="post">
+                <div class="form-group">
+                    <label for="title">Title</label>
+                    <input type="text" class="form-control" id="title" name="title">
+
+                </div>
+                <div class="form-group">
+                    <label for="content">content</label>
+                    <input type="text" class="form-control" id="content" name="content">
+                </div>
+                <button type="submit" class="btn btn-primary">submit</button>
+            </form>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.master')
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <a href="" class="btn btn-success">new Post</a>
+        </div>
+    </div>
+    <hr>
+    <div class="row">
+        <div class="col-md-12">
+            <p><strong>Learning Laravel outcomes</strong><a href="">Edit</a> </p>
+        </div>
+    </div>
+
+
+    @endsection
